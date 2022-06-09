4e6d23e8724cf7d2e49046f7a859e29786175b45
Chase <chasedemaster.tech@gmail.com>
2022-06-09 07:02:26 -0500

    commit tree 6a7e31d4971e9359b96c25b363b2b7915a4e560a

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+
+//based on the record data
+//have the denver broncos won the superbowl
+
+//look through the data
+    //function called superbowlWin 
+        //w a .find() method attatched to something 
+        // something about boolean if "w"
+
+
+function superbowlWin(array) {
+    let win = array.find((item) => item.result === "W")
+    if (win) {
+        return win.year
+    }else {
+        return "undefined"
+    }
+}
+
+superbowlWin(record)
